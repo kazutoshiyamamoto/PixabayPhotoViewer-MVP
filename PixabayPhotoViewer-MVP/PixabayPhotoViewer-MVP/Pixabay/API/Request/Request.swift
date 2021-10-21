2256a3b299004797e1c5275f0e9fdb853cc557da
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-21 22:31:30 +0900

    リクエスト処理に関わるプロトコルを追加

---
+++ PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/Pixabay/API/Request/Request.swift
@@ -0,0 +1,37 @@
+//
+//  Request.swift
+//  PixabayPhotoViewer-MVP
+//
+//  Created by home on 2021/10/20.
+//
+
+import Foundation
+
+protocol Request {
+    associatedtype Response: Decodable
+    
+    var baseURL: URL { get }
+    var method: HttpMethod { get }
+    var path: String { get }
+    var headerFields: [String: String] { get }
+    var queryParameters: [String: String]? { get }
+}
+
+extension Request {
+    var baseURL: URL {
+        return URL(string: "https://pixabay.com/api/")!
+    }
+    
+    var headerFields: [String: String] {
+        return ["Accept": "application/json"]
+    }
+    
+    var queryParameters: [String: String]? {
+        return nil
+    }
+}
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
