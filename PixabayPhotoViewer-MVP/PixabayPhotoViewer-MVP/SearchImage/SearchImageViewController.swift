a98fe1012d018333b86bf3a9e87c79c44b7a6590
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-25 22:47:14 +0900

    Presenterの初期化処理を追加

--- PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SearchImage/SearchImageViewController.swift
+++ PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SearchImage/SearchImageViewController.swift
@@ -12,6 +12,11 @@ class SearchImageViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    private var presenter: SearchImagePresenterInput!
+    func inject(presenter: SearchImagePresenterInput) {
+        self.presenter = presenter
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
