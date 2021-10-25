167fc3e8f0c3e858e9922266c9831aec2a9ef678
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-25 23:14:47 +0900

    Cellのコンテンツを事前にリセットする処理を追加

--- PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SearchImage/ImageCell.swift
+++ PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SearchImage/ImageCell.swift
@@ -12,15 +12,13 @@ class ImageCell: UITableViewCell {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        task?.cancel()
+        task = nil
+        imageView?.image = nil
     }
     
 }
