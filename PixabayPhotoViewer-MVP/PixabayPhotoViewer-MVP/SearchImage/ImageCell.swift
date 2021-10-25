584584fb82cc2470c0979fcbe149a0f0fbd2e5be
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-25 23:01:39 +0900

    検索画面で使用するCellを追加

---
+++ PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SearchImage/ImageCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageCell.swift
+//  PixabayPhotoViewer-MVP
+//
+//  Created by home on 2021/10/25.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
