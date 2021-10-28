be33b13f3511c62283ea55b2494cdd3b60ec40c3
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-10-28 18:56:30 +0900

    検索画面を生成する処理を追加

--- PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SceneDelegate.swift
+++ PixabayPhotoViewer-MVP/PixabayPhotoViewer-MVP/SceneDelegate.swift
@@ -16,7 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        let searchImageViewController = UIStoryboard(name: "SearchImage", bundle: nil).instantiateInitialViewController() as! SearchImageViewController
+        let navigationController = UINavigationController(rootViewController: searchImageViewController)
+
+        let model = SearchImageModel()
+        let presenter = SearchImagePresenter(view: searchImageViewController, model: model)
+        searchImageViewController.inject(presenter: presenter)
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
